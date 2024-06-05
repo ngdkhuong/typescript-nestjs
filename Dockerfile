fb7d54c4db7465d178d0e7a96f958ea1c51eb5ae
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-06-05 18:03:58 +0700

    added: docker compose

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+#build stage
+FROM node:20-alpine AS build
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . . 
+
+RUN npm run build
+
+#prod stage
+FROM node:20-alpine 
+
+WORKDIR /usr/src/app
+
+ARG NODE_ENV=production
+ENV NODE_ENV=${NODE_ENV}
+
+COPY --from=build /usr/src/app/dist ./dist
+
+RUN npm install --only=production
+
+RUN rm package*.json
+
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createDocument } from './swagger/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+import 'reflect-metadata';
+(async () => {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  SwaggerModule.setup('api/v1', app, createDocument(app));
+  await app.listen(process.env.PORT || 3000);
+  console.info('SERVER IS RUNNING ON PORT', process.env.PORT || 3000);
+})();
